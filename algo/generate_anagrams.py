7ed1922e09901fdc84da14a466b21581b8b68e84
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-07-11 11:39:33 -0700

    fix generate_anagrams

--- algo/generate_anagrams.py
+++ algo/generate_anagrams.py
@@ -1,10 +1,11 @@
 import sys
-import shuffle from random
+from random import shuffle 
 
 word = list(sys.argv[1])
 anagrams = []
 
 for i in range(10):
-    anagrams.append(''.join(shuffle(word)))
+    shuffle(word)
+    anagrams.append(''.join(word))
 
 print anagrams
