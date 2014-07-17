e64e946e1b25779f5ce8abd0f74287b750d1d4ab
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-07-17 16:56:58 -0700

    add optimized length_list

--- algo/length_list.py
+++ algo/length_list.py
@@ -1,10 +1,10 @@
-lists = [[1, 2, 3], [2, 5], [1], [1, 3, 9, 10]]
+lists = [[1, 2, 3], [2, 5], [1, 5, 5], [1, 3, 9, 10]]
 
 def find_min_list(lists):
     # lists is empty
     if len(lists) == 0:
         return
-    
+
     min = len(lists[0])
 
     # O(n*m) running time
@@ -14,4 +14,17 @@ def find_min_list(lists):
 
     return min
 
+def find_min_list2(lists):
+    i = 0
+    n = len(lists)
+
+    try:
+        while True:
+            for x in range(n):
+                _ = lists[x][i]
+            i += 1
+    except IndexError:
+        return i
+
 print find_min_list(lists)
+print find_min_list2(lists)
